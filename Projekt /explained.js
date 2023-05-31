1b913f6711aab7471457f122e78fe0f404bdc016
Roryop <roryop@tumo.world>
2023-05-31 19:45:32 +0200

    Spiel

---
+++ Projekt /explained.js
@@ -0,0 +1,14 @@
+/*
+
+
+Dieses Projekt funktioniert, indem sich verschiedene Lebewesen um die Daseinsberechtigung steiten.
+Vorhanden sind Gräser(grass), Grasfresser(grazer), Fleischfresser(grazerzer), und Zerstörer(Zerstorer).
+Außerdem existieren auch Häuser. In dem Spiel geht es darum, dass die Zerstorer die Häuser
+zerstören wollen. Da sich die Zerstorer aber nur auf Gras bewegen können, existieren die
+grazer, um das Gras zu zerstören, damit die Zerstorer aussterben. Um dem entgegenzuwirken existieren
+die grazerzer, welcher wiederum die Zerstorer unterstützen und die grazerzer ausschalten. Das Spiel ist zu Ende, sobald
+nur noch 2 neutrale oder verbündete Spielfiguren vorhanden sind,welche nicht im Konflikt miteinander stehen, oder nur noch Gras existiert.(Gras-Haus;Gras-Zerstorer;Gras)
+Dies ist meine Beschreibung für das Spiel.
+
+
+*/
---
+++ Projekt /script.js
@@ -0,0 +1,222 @@
+let matrix = [
+    [0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 2, 0],
+    [1, 1, 0, 0, 0]
+
+
+];
+
+
+let grassArr = [];
+let grazerArr = [];
+let grazerzerArr = [];
+let hausArr = [];
+let ZerstorerArr = [];
+
+
+
+
+let p = 10;
+
+let fr = 10;
+
+function randomMatrix(h,b){
+    let matrix = [];
+
+    for(let i = 0; i < h; i++){
+        matrix.push([]);
+        for(let l = 0; l < b; l++){
+            let b = random(-100,20);
+            if (b <= -50){
+                matrix[i].push(0);
+            } else if (b <= 20){
+                matrix[i].push(1);
+            }
+        }
+    }
+    for (let i = 0; i < matrix.length / 5; i++){
+        matrix[Math.floor(random(0, matrix.length - 1))][Math.floor(random(0, matrix[0].length - 1))] = 2;
+    }
+
+    for (let i = 0; i < matrix.length / 5; i++){
+        matrix[Math.floor(random(0, matrix.length - 1))][Math.floor(random(0, matrix[0].length - 1))] = 3;
+    }
+
+    for (let i = 0; i < matrix.length * 1.25; i++){
+        matrix[Math.floor(random(0, matrix.length - 1))][Math.floor(random(0, matrix[0].length - 1))] = 4;
+    }
+    for (let i = 0; i < matrix.length * 1.25; i++){
+        matrix[Math.floor(random(0, matrix.length - 1))][Math.floor(random(0, matrix[0].length - 1))] = 5;
+    }
+
+
+    console.log(matrix);
+
+    return matrix;
+}
+
+/*function randomMatrix(h,b){
+    let matrix = [];
+    let v = 7;
+
+    for(let i = 0; i < h; i++){
+        matrix.push([]);
+        for(let l = 0; l < b; l++){
+            if (l == v){
+                d = 1;
+                matrix[i].push(d);
+            } else {
+                d = 0;
+                matrix[i].push(d);
+            }
+        }
+        v -= 1;
+    }
+    console.log(matrix);
+
+    return matrix;
+}*/
+
+
+
+
+
+/*function randomMatrix(h,b){
+    let matrix = [];
+    let g = 1;
+
+    for(let i = 0; i < h; i++){
+        matrix.push([]);
+        for(let l = 0; l < b; l++){
+            if (g % 2 == 0){
+                d = 1;
+                matrix[i].push(d);
+            } else {
+                d = 0;
+                matrix[i].push(d);
+            }
+            g += 1;
+        }
+        g += 1;
+    }
+    console.log(matrix);
+
+    return matrix;
+}*/
+
+
+/*for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+        console.log(matrix[y][x]);
+    }
+}*/
+
+
+//wird am Start ausgeführt
+function setup(){
+    frameRate(fr);
+
+    matrix = randomMatrix(50,50);
+    createCanvas(matrix[0].length * p + 1, matrix.length * p + 1);
+    background(255,255,210);
+
+    for(let y = 0; y < matrix.length; y++){
+        for(let x = 0; x < matrix[y].length; x++){
+            if (matrix[y][x] === 1){
+                let GrassObj = new Grass(x,y);
+                console.log(GrassObj);
+                grassArr.push(GrassObj);
+            } else if (matrix[y][x] === 2){
+                let grazerObj = new Grazer(x,y);
+                console.log(grazerObj);
+                grazerArr.push(grazerObj);
+            } else if (matrix[y][x] === 3){
+                let grazerzerObj = new Grazerzer(x,y);
+                console.log(grazerzerObj);
+                grazerzerArr.push(grazerzerObj);
+            } else if (matrix[y][x] === 4){
+                let hausObj = new Haus(x,y);
+                console.log(hausObj);
+                hausArr.push(hausObj);
+            } else if (matrix[y][x] === 5){
+                let ZerstorerObj = new Zerstorer(x,y);
+                console.log(ZerstorerObj);
+                ZerstorerArr.push(ZerstorerObj);
+            }
+        }
+    }
+
+
+
+
+
+    /*let Grass1 = new Grass(1,2);
+    console.log(Grass1);
+    console.log(Grass1.findFields(0));
+    //Grass1.markieren();*/
+}
+
+//wird perma ausgeführt
+function draw(){
+
+    for(let i = 0; i < matrix.length; i++){
+        for(let x = 0; x < matrix[i].length; x++){
+            if (matrix[i][x] == 0){
+                fill("white");
+            } else if (matrix[i][x] == 1){
+                fill("green");
+            } else if (matrix[i][x] == 2){
+                fill("yellow");
+            } else if (matrix[i][x] == 3){
+                fill("red");
+            } else if (matrix[i][x] == 4){
+                fill("black");
+            } else if (matrix[i][x] == 5){
+                fill("blue");
+            }
+            rect(x * p, i * p, p, p);
+        }
+    }
+
+
+
+    for (let i = 0; i < grassArr.length; i++) {
+        let grassObj = grassArr[i];
+        grassObj.mul();
+        
+    }
+
+    for (let i = 0; i < grazerArr.length; i++) {
+        let grazerObj = grazerArr[i];
+        grazerObj.eat();
+        grazerObj.mul();
+        console.log(grazerArr[0]);
+        grazerObj.die();
+        
+    }
+
+    for (let i = 0; i < grazerzerArr.length; i++) {
+        let grazerzerObj = grazerzerArr[i];
+        grazerzerObj.eat();
+        grazerzerObj.mul();
+        grazerzerObj.die();
+        
+    }
+
+    for (let i = 0; i < ZerstorerArr.length; i++) {
+        let ZerstorerObj = ZerstorerArr[i];
+        //ZerstorerObj.teleport();
+        ZerstorerObj.eat();
+        ZerstorerObj.die();
+
+        
+    }
+
+
+
+
+}
